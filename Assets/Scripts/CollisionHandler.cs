7ae877feba656f656eaf938b7b3f6e036b27f4a8
Callum Owen <callumowen6@googlemail.com>
2021-11-28 16:16:08 +0000

    Explosion effect, laser particle hit disables enemies, player crash tiggers scene reload

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{    
+    [SerializeField] float levelLoadDelay = 1f;
+
+    bool isTransitioning = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isTransitioning) { return; }
+
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
