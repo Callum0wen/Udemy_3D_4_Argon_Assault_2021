bbfacf5fe864346e966395a76267c345d4063e7f
Callum Owen <callumowen6@googlemail.com>
2021-11-28 18:29:35 +0000

    Enemy Health, scoreboard, on hit particle VFX and ship factory

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {    
     [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
 
     bool isTransitioning = false;
 
@@ -19,6 +20,9 @@ public class CollisionHandler : MonoBehaviour
     void StartCrashSequence()
     {
         isTransitioning = true;
+        crashVFX.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
         GetComponent<PlayerController>().enabled = false;
         Invoke("ReloadLevel", levelLoadDelay);
     }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 4;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name} hit by {other.gameObject.name}");
+        ProcessHit();
+
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+
 }
